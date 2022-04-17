e9026b1e22255ef213f8daa3d6746ef636d383a9
Alan Shirk <ashirk94@gmail.com>
2022-04-16 21:02:06 -0700

    urlmaker to mapfeature

--- MapFeature.js
+++ MapFeature.js
@@ -1,98 +1,95 @@
+import UrlMarker from "./markers/UrlMarker";
+
 const MapFeature = (function () {
+  function MapFeature(feature) {
+    this.isInitialized = false;
+    this.name = feature.name;
+    this.label = feature.label;
+    this.data = feature.data || [];
+    this.markers = [];
+    this.markerStyle = feature.markerStyle;
+    this.status = feature.status;
+    this.datasource = feature.datasource && feature.datasource.bind(this);
+  }
 
-	function MapFeature(feature) {
-		this.isInitialized = false;
-		this.name = feature.name;
-		this.label = feature.label;
-		this.data = feature.data || [];
-		this.markers = [];
-		this.markerStyle = feature.markerStyle;
-		this.status = feature.status;
-		this.datasource = feature.datasource && feature.datasource.bind(this);
-	}
+  function setMap(map) {
+    this.map = map;
+  }
+  /*
+   * Consistently returns Promises for use with .then().
+   */
+  function loadData() {
+    this.data = this.datasource();
+    return this.data;
+  }
 
-    function setMap(map) {
-        this.map = map;
-    }
-	/*
-	 * Consistently returns Promises for use with .then().
-	 */
-	function loadData() {
-		this.data = this.datasource();
-		return this.data;
-	}
+  function initialize() {
+    this.isInitialized = true;
+    this.loadData();
+    this.markers = this.loadMarkers();
+  }
 
-	function initialize() {
-		this.isInitialized = true;
-		this.loadData();
-		this.markers = this.loadMarkers();
-	}
+  function render(targetMap) {
+    this.markers.forEach(function (marker) {
+      marker.setMap(targetMap);
+    });
+  }
 
+  function clone() {
+    return new MapFeature(this.config);
+  }
 
-	function render(targetMap) {
-		this.markers.forEach(function (marker) { marker.setMap(targetMap); });
-	}
+  function hide() {
+    this.markers.forEach(function (marker) {
+      marker.setMap(null);
+    });
+  }
 
-	function clone() {
-		return new MapFeature(this.config)
-	}
+  function getMarkers() {
+    return this.markers;
+  }
 
-	function hide() {
-		this.markers.forEach(function (marker) {
-			marker.setMap(null);
-		});
-	}
+  function loadMarkers() {
+    return this.data.then((sources) => {
+      let errors = [];
 
+      for (let i = 0; i < sources.length; i++) {
+        // Get the dataset
+        let item = sources[i];
+        let label = this.getLabel();
+        if (item.position.lat == null) continue;
 
-	function getMarkers() {
-		return this.markers;
-	}
+        // Set the source for the marker URL
+        item.markerUrl = this.markerStyle;
 
-	function loadMarkers(){
-        return this.data.then((sources) =>{
-			let errors =[];
+        let urlMarker = new UrlMarker(item);
+        let googleMarker = urlMarker.createMarker();
 
-			for (let i = 0; i < sources.length; i++) {
-				// Get the dataset
-				let item = sources[i];
-				let label = this.getLabel();
-				if (item.position.lat == null) continue;
-					
-				// Set the source for the marker URL
-				item.markerUrl = this.markerStyle;
+        //Push the new marker to the marker array
 
+        this.markers.push(googleMarker);
+      }
+    });
+  }
 
-				let urlMarker = new UrlMarker(item);
-				let googleMarker = urlMarker.createMarker();
+  function getLabel() {
+    return this.markerLabel;
+  }
 
-				
-				//Push the new marker to the marker array
-				 
-				this.markers.push(googleMarker);
-			}
-		});
-	}
+  var prototype = {
+    loadData: loadData,
+    loadMarkers: loadMarkers,
+    getLabel: getLabel,
+    setMap: setMap,
+    getMarkers: getMarkers,
+    render: render,
+    hide: hide,
+    initialize: initialize,
+  };
 
-	function getLabel()
-	{
-		return this.markerLabel;
-	}
+  MapFeature.prototype = prototype;
 
-
-	var prototype = {
-		loadData: loadData,
-		loadMarkers: loadMarkers,
-		getLabel: getLabel,
-        setMap: setMap,
-		getMarkers: getMarkers,
-		render: render,
-		hide: hide,
-		initialize: initialize
-	};
-
-	MapFeature.prototype = prototype;
-
-	return MapFeature;
+  return MapFeature;
 })();
 
 export default MapFeature;
